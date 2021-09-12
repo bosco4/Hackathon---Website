b97b0daccf5ce65a102846cc949316a730bff1f7
Laptop <junhyeon@ualberta.ca>
2021-09-12 08:51:58 -0600

    Merging Alex's work Applied Django backend solution to the project

---
+++ mysite/polls/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/details', views.details, name='details'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    path('game_breath/', views.game_breath, name='game_breath'),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
